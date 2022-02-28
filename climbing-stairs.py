72979002d96c10bfc33f29f0f9ee7dba600f9821
jackmford <jackmitchellfordyce@gmail.com>
2022-02-28 15:19:44 -0600

    fib

---
+++ climbing-stairs.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# 0 = 0
+# 1 = 1
+# 2 = 2
+# 3 = 3
+# 4 = 5
+class Solution:
+    def climbStairs(self, n: int) -> int:
+      first, second = 0, 1
+
+      for i in range(n):
+        first, second = second, first+second
+      return second
+
+sol = Solution()
+print(sol.climbStairs(2))
