671254ffde7d330c2b4bcb1bb96c386fac4b55af
jackmford <jackmitchellfordyce@gmail.com>
2022-03-02 18:28:09 -0600

    not right

--- coin-change.py
+++ coin-change.py
@@ -2,16 +2,23 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+      if amount == 0:
+        return 0
+
       total = 0
       cur_index = 0
       ctr = 0
       coins.sort(reverse=True)
       print(coins)
+      print(coins[cur_index]%amount)
 
-      if len(coins) == 1 and coins[cur_index] < amount:
+      if len(coins) == 1 and amount%coins[cur_index] != 0:
+        print('mod')
         return -1
 
       while total < amount:
+        print(total)
+        print(cur_index)
         if total+coins[cur_index] <= amount:
           total+=coins[cur_index]
           ctr+=1
@@ -25,4 +32,4 @@ class Solution:
         return -1
 
 sol = Solution()
-print(sol.coinChange([1, 2, 5], 11))
+print(sol.coinChange([186,419,83,408] , 6249))
