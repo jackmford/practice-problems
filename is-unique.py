fe2a58ad0747acb53399376f2b14f8f40d3ea8b6
jackmford <jackmitchellfordyce@gmail.com>
2022-03-15 15:27:29 -0500

    sets

--- is-unique.py
+++ is-unique.py
@@ -1,13 +1,28 @@
 def brute_force(chars):
-  print(chars[0])
   for i in range(len(chars)):
     for j in range(i+1, len(chars)):
       if chars[i] == chars[j]:
-        return True
-  return False
+        return False
+  return True
+
+def set_approach(chars):
+  char_set = set()
+  for char in chars:
+    char_set.add(char)
+
+  if len(chars) == len(char_set):
+    return True
+  else:
+    return False
 
 result = brute_force('aabcdefg')
-if result is True:
+if result is False:
   print("Not all unique.")
 else:
   print("All unique.")
+
+result = set_approach('abcdefgg')
+if result is True:
+  print("All unique.")
+else:
+  print("Not all unique.")
