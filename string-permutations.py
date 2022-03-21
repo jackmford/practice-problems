ea8c04d9f88e82fdbc2dcc461edc834c20bf8692
jackmford <jackmitchellfordyce@gmail.com>
2022-03-21 15:26:50 -0500

    nifty dict solution

--- string-permutations.py
+++ string-permutations.py
@@ -11,7 +11,27 @@ def find_permutation(string_one, string_two):
 
   return True
 
-permutation = find_permutation('hi', 'ihi')
+def find_permutation_2(string_one, string_two):
+  if len(string_one) != len(string_two):
+    return False
+
+  counts = {}
+  for char in string_one:
+    counts[char] = 0
+
+  for char in string_one:
+    counts[char]+=1
+
+  for char in string_two:
+    counts[char]-=1
+    
+  for char in counts:
+    if counts[char] != 0:
+      return False
+
+  return True
+
+permutation = find_permutation_2('hi', 'hih')
 if permutation is False:
   print("Not a permutation.")
 else:
