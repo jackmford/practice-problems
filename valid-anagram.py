b81798c5d38417468656bb5302e47432965b45d9
jackmford <jackmitchellfordyce@gmail.com>
2022-04-12 16:09:21 -0500

    valid anagram

---
+++ valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    s_dict = {}
+    t_dict = {}
+
+    if len(s) != len(t):
+      return False
+
+    s = ''.join(sorted(s))
+    t = ''.join(sorted(t))
+
+    for i in range(len(s)):
+      if s[i] != t[i]:
+        return False
+
+    return True
+
+sol = Solution()
+sol.isAnagram('hi', 'ih')
