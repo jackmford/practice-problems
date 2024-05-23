f62ea6b782423a03224687a458054c6d12b57093
jackmford <jackmitchellfordyce@gmail.com>
2024-05-23 17:13:45 -0500

    replace elements w greatest

--- 2024-mk2/arrays/replace-element-with-greatest-element-on-right-side.py
+++ 2024-mk2/arrays/replace-element-with-greatest-element-on-right-side.py
@@ -7,4 +7,16 @@ After doing so, return the array.
 from typing import List
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
+        # initial max is -1, base case
+        # go through in reverse order
+        # setting arr[i] to the max of rightMax
+        rightMax = -1
+        for i in range(len(arr)-1, -1, -1):
+            tmpMax = max(arr[i], rightMax)
+            arr[i] = rightMax
+            rightMax = tmpMax
+        return arr 
+
+sol = Solution()
+print(sol.replaceElements([17,18,5,4,6,1]))
 
