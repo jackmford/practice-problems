5c849c1676b8b8bd4555b1ed61bdbc5adc68144f
jackmford <jackmitchellfordyce@gmail.com>
2024-06-12 16:25:13 -0500

    longest common practice

--- 2024-mk2/arrays/longest-common-prefix.py
+++ 2024-mk2/arrays/longest-common-prefix.py
@@ -7,22 +7,24 @@ If there is no common prefix, return an empty string "".
 from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        min = len(strs[0])
-        prefix = ''
-        for word in strs:
-            if len(word) < min:
-                min = len(word)
-        i = 0
-        prefix = ''
-        while i < min:
-            c = strs[0][i]
-            for word in strs:
-                if word[i] != c:
-                    return prefix
-            prefix+=strs[0][i]
-            i+=1
+        res = ""
 
-        return prefix
+        min_len = len(strs[0])
+        for word in strs:
+            min_len = min(min_len, len(word))
+        
+        i = 0
+        res = ""
+
+        while i < min_len:
+            char = strs[0][i]
+
+            for word in strs:
+                if word[i] != char:
+                    return res
+
+            res += char
+            i+=1
 
 sol = Solution()
 print(sol.longestCommonPrefix(["flower","flow","flight"]))
