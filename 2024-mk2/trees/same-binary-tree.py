601bf844eb67c36dbfa36150fbfe03a1df12ca3d
jackmford <jackmitchellfordyce@gmail.com>
2024-07-08 15:41:14 -0500

    same binary tree

---
+++ 2024-mk2/trees/same-binary-tree.py
@@ -0,0 +1,31 @@
+"""
+Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+
+Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        self.res = True
+        def dfs(proot, qroot):
+            if proot == None and qroot == None:
+                return
+            if proot == None or qroot == None:
+                self.res = False
+                return
+            if proot.val != qroot.val:
+                self.res = False
+            dfs(proot.left, qroot.left)
+            dfs(proot.right, qroot.right)
+            return
+
+        dfs(p, q)
+        return self.res
+
+            
