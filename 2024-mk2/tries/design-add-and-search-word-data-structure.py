12eaed1fa691918a0e14bef98b2dc267a701087f
jackmford <jackmitchellfordyce@gmail.com>
2025-01-05 15:00:28 -0600

    trie practice

--- 2024-mk2/tries/design-add-and-search-word-data-structure.py
+++ 2024-mk2/tries/design-add-and-search-word-data-structure.py
@@ -1,43 +1,40 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self.m = {}
         self.end = False
-        
 class WordDictionary:
     def __init__(self):
         self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         cur = self.root
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
+
+        for char in word:
+            if char not in cur.m:
+                cur.m[char] = TrieNode()
+            cur = cur.m[char]
+        
         cur.end = True
 
     def search(self, word: str) -> bool:
 
-        def searchFromLocation(i, cur):
+        # This function searches from any index for validity
+        def searchRemainder(cur, i):
             for j in range(i, len(word)):
-                print(word[j])
                 if word[j] == ".":
-                    # If found "." need to check all cur's children for matching char
-                    for child in cur.children:
-                        valid = searchFromLocation(j+1,cur.children[child])
-                        if valid:
+                    # For every option in cur ("." == any character)
+                    for c in cur.m:
+                        if searchRemainder(cur.m[c], j+1):
                             return True
-
-                    # There are no more children but "." is still present, there can't possibly be a match.
+                    # There are no more characters to check, found no valid word
                     return False
+                if word[j] in cur.m:
+                    cur = cur.m[word[j]]
                 else:
-                    if word[j] not in cur.children:
-                        return False
-                    cur = cur.children[word[j]] 
+                    return False
             return cur.end
-            
-        return searchFromLocation(0, self.root)
-        
 
+        return searchRemainder(self.root, 0)
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
