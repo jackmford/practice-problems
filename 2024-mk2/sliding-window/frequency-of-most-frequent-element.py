56ea6621e277076e3d32f3b1246702208157399d
jackmford <jackmitchellfordyce@gmail.com>
2025-01-11 10:27:44 -0600

    freq of most feq el practice

--- 2024-mk2/sliding-window/frequency-of-most-frequent-element.py
+++ 2024-mk2/sliding-window/frequency-of-most-frequent-element.py
@@ -1,27 +1,20 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        res = 0
-        l, r = 0, 0
-        total = 0
+        
         nums = sorted(nums)
-        print(nums)
+        l,r = 0,0
+        res = 0
+        total = 0
 
         while r < len(nums):
             total+=nums[r]
 
-            # Is window valid?
-            # If not, shrink it
-            # If window is valid record window size and move the right pointer
-            # Always want to check right away if the last addition of the right pointer made window invalid
-            # instead of just adding right away and then recording
-            while ((nums[r]*(r-l+1)) - total) > k:
+            # Check for validity
+            while (r-l+1)*nums[r] - total > k:
                 total-=nums[l]
                 l+=1
-
-            # Once we have a valid window, record window size and move the right pointer
-            res = max(r-l+1, res)
+            
+            res = max(res, (r-l+1))
             r+=1
-
-
         return res
-        
+
