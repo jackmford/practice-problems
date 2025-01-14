2441cb19017bb1a81cfa0792ac30cce0a26a37c6
jackmford <jackmitchellfordyce@gmail.com>
2025-01-13 18:58:14 -0600

    min cost practice

--- 2024-mk2/graphs/min-clost-to-connect-all-points.py
+++ 2024-mk2/graphs/min-clost-to-connect-all-points.py
@@ -29,3 +29,31 @@ class Solution:
 
 
         
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        
+        q = []
+        visited = set()
+        heapq.heappush(q, (0, 0))
+        res = 0
+
+        # Starting from anywhere,
+        # visit the shortest path first until all the nodes are visited
+
+        while len(visited) < len(points):
+            cur = heapq.heappop(q)
+            cost = cur[0]
+            idx = cur[1]
+            curPoint = points[idx]
+            # Already have been to this node, don't need to recheck
+            if idx in visited:
+                continue
+            
+            res += cost
+            visited.add(idx)
+            for i in range(len(points)):
+                dist = abs((curPoint[0] - points[i][0])) + abs((curPoint[1] - points[i][1]))
+                if i != idx and i not in visited:
+                    heapq.heappush(q, (dist, i))
+        return res
+
