bcbd8d1a4da4148401d1ffc98c3bd2e130b0e510
jackmford <jackmitchellfordyce@gmail.com>
2025-01-20 10:54:06 -0600

    partition equal subset sum memoization

--- 2024-mk2/dp/partition-equal-subset-sum.py
+++ 2024-mk2/dp/partition-equal-subset-sum.py
@@ -1,24 +1,32 @@
-        
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums)%2 != 0:
+        # Basically need a path that adds up to sum(nums)//2
+        if sum(nums) % 2 != 0:
             return False
-
-        targ = sum(nums)//2
-        def dfs(i, total):
-            if i >= len(nums):
-                if total == targ:
-                    return True
+        half = sum(nums)//2 
+        print(half)
+        # Can choose/not choose a number
+        memo = [[-1]*(half+1) for _ in range(len(nums)+1)]
+        def dfs(total, i):
+            print(total)
+            if total == half:
+                return True
+            if total > half:
                 return False
-            if total > targ:
-                return False
+            if memo[i][total] != -1:
+                return memo[i][total]
             
-            # Choose
-            c = dfs(i+1, total+nums[i])
-            # Don't choose
-            d = dfs(i+1, total)
+            pick = False
+            dont = False
+            if i < len(nums):
+                # Pick
+                pick = dfs(total+nums[i], i+1)
+                # Dont pick
+                dont = dfs(total, i+1)
 
-            return c or d
-
-        return dfs(0, 0)
+            memo[i][total] = pick or dont
+            return pick or dont
         
+        return dfs(0, 0)
+
+
