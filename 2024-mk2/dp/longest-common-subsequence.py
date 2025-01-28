500d5fcf9166790581e39b9b7ff44106935c4058
jackmford <jackmitchellfordyce@gmail.com>
2025-01-28 16:05:49 -0600

    practice longest common sub

--- 2024-mk2/dp/longest-common-subsequence.py
+++ 2024-mk2/dp/longest-common-subsequence.py
@@ -21,3 +21,28 @@ class Solution:
 
         return dfs(0, 0)
 
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # Have two pointers
+        # If both are the same character, move them 1 and return 1
+        # If they are not, move both independently of one another
+
+        memo = {}
+        def dfs(i, j):
+            if i >= len(text1) or j >= len(text2):
+                return 0
+            if (i, j) in memo:
+                return memo[(i,j)]
+            if text1[i] == text2[j]:
+                return 1 + dfs(i+1, j+1)
+            
+            move_i = dfs(i+1, j)
+            move_j = dfs(i, j+1)
+
+            memo[(i, j)] = max(move_i, move_j)
+        
+
+            return max(move_i, move_j)
+        
+        return dfs(0, 0)
+
