1528cfb59a00045e1693bd43ddb217ef57d3f7fc
jackmford <jackmitchellfordyce@gmail.com>
2025-02-03 16:33:41 -0600

    min cost climbing stairs practice

--- 2024-mk2/dp/min-cost-climbing-stairs.py
+++ 2024-mk2/dp/min-cost-climbing-stairs.py
@@ -21,4 +21,25 @@ class Solution:
         return res
 
             
-        
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # Can take the current step and add it and go to next
+        # Or skip it and go i+2
+
+        memo = {}
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            if i in memo:
+                return memo[i]
+
+            # From where we are we can choose to climb or skip
+            one = dfs(i+1)
+            two = dfs(i+2)
+
+            memo[i] = cost[i]+min(one,two)
+
+            return memo[i]
+
+        return min(dfs(0), dfs(1))
+                
