66719105e0b7a7abbd2e746e4495befa5412ca1b
jackmford <jackmitchellfordyce@gmail.com>
2025-02-04 16:55:17 -0600

    rotting oranges practice

--- 2024-mk2/graphs/rotting-oranges.py
+++ 2024-mk2/graphs/rotting-oranges.py
@@ -41,4 +41,54 @@ class Solution:
             res+=1
 
         return res if fresh == 0 else -1
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # This is a level by level traversal from every "2"
 
+        q = deque()
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        fresh = 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh+=1
+                if grid[r][c] == 2:
+                    q.append((r,c))
+
+        ctr = 0
+        while fresh > 0 and q:
+            qLen = len(q)
+            print(qLen)
+            print(grid)
+
+            for _ in range(qLen):
+                cur = q.popleft()
+                if cur in visited:
+                    continue
+    
+                visited.add(cur)
+                r, c = cur[0], cur[1]
+                if r > 0 and grid[r-1][c] == 1:
+                    grid[r-1][c] = 2
+                    q.append((r-1, c))
+                    fresh-=1
+                if r < ROWS-1 and grid[r+1][c] == 1:
+                    grid[r+1][c] = 2
+                    q.append((r+1, c))
+                    fresh-=1
+                if c > 0 and grid[r][c-1] == 1:
+                    grid[r][c-1] = 2
+                    q.append((r, c-1))
+                    fresh-=1
+                if c < COLS-1 and grid[r][c+1] == 1:
+                    grid[r][c+1] = 2
+                    q.append((r, c+1))
+                    fresh-=1
+                # Process cur's neighbors
+            ctr+=1
+
+        if fresh > 0:
+            return -1
+        return ctr
