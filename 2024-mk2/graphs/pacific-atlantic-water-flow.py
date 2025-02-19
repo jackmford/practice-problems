1c295bf28bf03dfddaa2f46e3ef3d99048bccb6b
jackmford <jackmitchellfordyce@gmail.com>
2025-02-19 15:12:37 -0600

    pacific atlantic practice

--- 2024-mk2/graphs/pacific-atlantic-water-flow.py
+++ 2024-mk2/graphs/pacific-atlantic-water-flow.py
@@ -47,4 +47,47 @@ class Solution:
 
             
             
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # From the top and left side, find any node that can reach the pacific
+        # From the right and bottom side, find any node that can reach the atlantic
+        # Nodes in both are to be returned
 
+        atlantic, pacific = set(), set()
+        ROWS, COLS = len(heights), len(heights[0])
+
+        def dfs(x, y, visited):
+            if (x, y) in visited:
+                return
+            
+            # Add the node we are on to the visited set, meaning we can get to this ocean from this node
+            visited.add((x,y))
+            # Check every direction for more nodes to visit
+            if x > 0 and heights[x-1][y] >= heights[x][y]:
+                dfs(x-1, y, visited)
+            if x < ROWS-1 and heights[x+1][y] >= heights[x][y]:
+                dfs(x+1, y, visited)
+            if y > 0 and heights[x][y-1] >= heights[x][y]:
+                dfs(x, y-1, visited)
+            if y < COLS-1 and heights[x][y+1] >= heights[x][y]:
+                dfs(x, y+1, visited)
+
+        # Left and right
+        for r in range(ROWS):
+            dfs(r, 0, pacific)
+            dfs(r, COLS-1, atlantic)
+        # Top and bottom
+        for c in range(COLS):
+            dfs(0, c, pacific)
+            dfs(ROWS-1, c, atlantic)
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r,c) in pacific and (r,c) in atlantic:
+                    res.append([r, c])
+                    
+        return res
+
+
+        
