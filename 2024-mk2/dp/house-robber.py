92d6cd946912fe08dcb36f29e02bfa27c8210de2
jackmford <jackmitchellfordyce@gmail.com>
2025-02-20 15:48:21 -0600

    house robber prac

--- 2024-mk2/dp/house-robber.py
+++ 2024-mk2/dp/house-robber.py
@@ -22,4 +22,22 @@ class Solution:
 
 
 
-        
+ class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # we can either rob a house and skip the next
+        # or we can skip the current and move to the next
+        memo = {}
+        def rob(i):
+            if i >= len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
+
+            take = nums[i] + rob(i+2)
+            skip = rob(i+1)
+
+            memo[i] = max(take, skip)
+            return memo[i]
+
+        return rob(0)
+               
