5b84b21d9c0ab25b13eddeda58726ee7ac0dfb85
jackmford <jackmitchellfordyce@gmail.com>
2026-03-05 17:38:00 -0500

    fmt

--- 2026/polygon-mon/producer-service/poly-producer.py
+++ 2026/polygon-mon/producer-service/poly-producer.py
@@ -12,11 +12,14 @@ REDIS_CONN = Redis(host="redis", port=6379)
 # Using a queue here but for multiple consumers could use
 # Redis STREAM instead
 queue = Queue(connection=REDIS_CONN)
-QUEUE_NAME = 'QUEUE'
+QUEUE_NAME = "QUEUE"
 RUN_TIME = 5
 PROMETHEUS_PORT = 8005
 
-QUEUE_DEPTH = Gauge('transaction_queue_depth', 'Current depth of the Redis queue holding transactions')
+QUEUE_DEPTH = Gauge(
+    "transaction_queue_depth", "Current depth of the Redis queue holding transactions"
+)
+
 
 # Make func to randomly push onto redis queue
 # This simulates MATCHED transactions coming from the DB ready to be
@@ -24,17 +27,18 @@ QUEUE_DEPTH = Gauge('transaction_queue_depth', 'Current depth of the Redis queue
 def push_transaction():
 
     print(REDIS_CONN.llen(QUEUE_NAME))
-    QUEUE_DEPTH.set(REDIS_CONN.llen(QUEUE_NAME))
+    QUEUE_DEPTH.set(REDIS_CONN.llen(QUEUE_NAME))  # type:ignore
     # Push something half the time
     data = {
-        "transaction_id": str(uuid.uuid4),
-        "maker_id":  str(uuid.uuid4),
-        "taker_id":  str(uuid.uuid4),
+        "transaction_id": str(uuid.uuid4()),
+        "maker_id": str(uuid.uuid4()),
+        "taker_id": str(uuid.uuid4()),
         "state": "MATCHED",
     }
     if random.random() < 0.5:
         print("Pushing transaction to queue")
         REDIS_CONN.rpush(QUEUE_NAME, json.dumps(data))
+
 
 def main():
     start_http_server(PROMETHEUS_PORT)
@@ -43,6 +47,7 @@ def main():
         push_transaction()
         time.sleep(RUN_TIME)
     return
+
 
 if __name__ == "__main__":
     main()
