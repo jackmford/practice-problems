8f8221aaca1d00b1ee61ad8c459193d4e2c905a7
jackmford <jackmitchellfordyce@gmail.com>
2026-03-06 17:13:18 -0500

    switch to async

--- 2026/polygon-mon/relay-service/poly-relay.py
+++ 2026/polygon-mon/relay-service/poly-relay.py
@@ -1,13 +1,12 @@
 import json
-import time
-import threading
+import asyncio
 import random
 
 from redis import Redis
 from rq import Queue
 from prometheus_client import start_http_server, Counter, Histogram
 
-lock = threading.Lock()
+lock = asyncio.Lock()
 
 REDIS_CONN = Redis(host="redis", port=6379, decode_responses=True)
 
@@ -35,27 +34,33 @@ REQUEST_DURATION = Histogram(
     buckets=[0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0],
 )
 
+TRANSACTION_MAX_RETRIES = 3
+RETRY_BASE_DELAY = 0.25
 
-def poll_for_transactions():
+
+async def poll_for_transactions():
     while True:
         item = REDIS_CONN.rpop(QUEUE_NAME)
         if item:
             print("Pulling transaction from queue")
             try:
                 item_json = json.loads(item)  # type:ignore
-                with lock:
+                async with lock:
                     TRANSACTIONS_TO_SEND[item_json["transaction_id"]] = item_json
+                    TRANSACTIONS_TO_SEND[item_json["transaction_id"]]["state"] = (
+                        "PENDING"
+                    )
 
             except Exception as e:
-                print(f"Exception processing item {item.decode('utf-8')}: {e}")  # type:ignore
+                print(f"Exception processing item {item}: {e}")  # type:ignore
 
-        time.sleep(POLL_TIME)
+        await asyncio.sleep(POLL_TIME)
 
 
 # Mock API
-def poly_request():
+async def poly_request():
     # Generate latency data for our histogram
-    time.sleep(random.randint(0, 12))
+    await asyncio.sleep(random.randint(0, 12))
 
     if random.random() < 0.3:
         return 500
@@ -63,60 +68,93 @@ def poly_request():
     return 200
 
 
-def send_transactions_to_poly():
-    while True:
-        with lock:
-            # These keys should all be marked as IN PROGRESS so no other threads pick them up
-            # Could use a temporary function scoped dict, copy them over, remove them from main, submit failed ones back onto main dict
-            keys = list(TRANSACTIONS_TO_SEND.keys())
-
-        for transaction_id in keys:
+async def send_request(transaction_id):
+    # these should all be sent async
+    print(f"Sending {transaction_id} to Poly...")
+    for attempt in range(TRANSACTION_MAX_RETRIES):
+        try:
             with REQUEST_DURATION.time():
-                response = poly_request()
+                response = await poly_request()
 
+            # Use raise_for_status() on a real request
             if response == 200:
                 # Transaction is "ACCEPTED", but not "FINALIZED" yet
                 # Indexer service handles watching for FINALIZED and
                 # Updating DB
-                print(f"{transaction_id} accepted.")
-                ACCEPTED_TRANSACTIONS.inc()
-                with lock:
+                async with lock:
                     if transaction_id in TRANSACTIONS_TO_SEND:
+                        print(f"{transaction_id} accepted on attempt {attempt + 1}.")
+                        ACCEPTED_TRANSACTIONS.inc()
                         del TRANSACTIONS_TO_SEND[transaction_id]
+                break
             else:
-                FAILED_TRANSACTIONS.inc()
+                raise Exception("Request to poly_request failed.")
+        except Exception as e:
+            FAILED_TRANSACTIONS.inc()
+            if attempt == TRANSACTION_MAX_RETRIES - 1:
+                print(
+                    f"{transaction_id} attempt {attempt + 1}/{TRANSACTION_MAX_RETRIES} failed: {e}. Marking as failed in DB"
+                )
+                # Mark as failed in DB MOCK
+                break
+
+            print(
+                f"{transaction_id} attempt {attempt + 1}/{TRANSACTION_MAX_RETRIES} failed: {e}. Retrying..."
+            )
+
+            # TRANSACTIONS_TO_SEND[transaction_id].setdefault("retries", 0)
+            # TRANSACTIONS_TO_SEND[transaction_id]["retries"] += 1
+
+            # exponential backoff
+            delay = RETRY_BASE_DELAY * (2**attempt)
+            # jitter
+            delay *= random.uniform(0.8, 1.2)
+
+            await asyncio.sleep(delay)
+
+
+async def send_transactions_to_poly():
+    while True:
+        pending_to_send = []
+        async with lock:
+            # These keys should all be marked as IN PROGRESS so no other threads pick them up
+            # Could use a temporary function scoped dict, copy them over, remove them from main, submit failed ones back onto main dict
+            keys = list(TRANSACTIONS_TO_SEND.keys())
+            for transaction_id in keys:
                 if (
-                    "retries" in TRANSACTIONS_TO_SEND[transaction_id]
-                    and TRANSACTIONS_TO_SEND[transaction_id]["retries"] >= 3
+                    transaction_id in TRANSACTIONS_TO_SEND
+                    and TRANSACTIONS_TO_SEND[transaction_id]["state"] == "PENDING"
                 ):
-                    print(
-                        f"{transaction_id} failed. Retries exceeded. Marking as failed in DB."
-                    )
-                    # Mark as failed in DB MOCK
+                    TRANSACTIONS_TO_SEND[transaction_id]["state"] = "IN_PROGRESS"
+                    pending_to_send.append(transaction_id)
 
-                print(f"{transaction_id} failed. Retrying.")
-                TRANSACTIONS_TO_SEND[transaction_id].setdefault("retries", 0)
-                TRANSACTIONS_TO_SEND[transaction_id]["retries"] += 1
+        # Concurrently execute these
+        tasks = []
+        for transaction_id in pending_to_send:
+            tasks.append(send_request(transaction_id))
 
-        time.sleep(SEND_TIME)
+        await asyncio.gather(*tasks)
+
+        await asyncio.sleep(SEND_TIME)
 
 
-def main():
+async def main():
     start_http_server(PROMETHEUS_PORT)
-    print("hi")
-    poll_thread = threading.Thread(target=poll_for_transactions)
-    poll_thread.start()
+    await asyncio.gather(poll_for_transactions(), send_transactions_to_poly())
 
-    send_thread = threading.Thread(target=send_transactions_to_poly)
-    send_thread.start()
+    # poll_thread = threading.Thread(target=poll_for_transactions)
+    # poll_thread.start()
+
+    # send_thread = threading.Thread(target=send_transactions_to_poly)
+    # send_thread.start()
 
     # Wait for threads to complete before the main program exits
-    poll_thread.join()
-    send_thread.join()
+    # poll_thread.join()
+    # send_thread.join()
 
     print("Main thread finished.")
     return
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
