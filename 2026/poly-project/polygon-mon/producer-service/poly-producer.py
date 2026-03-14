9d85d2dccec73d5cfa595dc862f3a02d36224ad2
jackmford <jackmitchellfordyce@gmail.com>
2026-03-14 10:47:26 -0400

    async producer

--- 2026/poly-project/polygon-mon/producer-service/poly-producer.py
+++ 2026/poly-project/polygon-mon/producer-service/poly-producer.py
@@ -1,10 +1,10 @@
+import asyncio
 import json
 import structlog
 import random
-import time
 import uuid
 
-from prometheus_client import start_http_server, Gauge, Info
+from prometheus_client import start_http_server, Gauge
 from redis import Redis
 from rq import Queue
 
@@ -36,37 +36,36 @@ QUEUE_DEPTH = Gauge(
 # Make func to randomly push onto redis queue
 # This simulates MATCHED transactions coming from the DB ready to be
 # Sent to Polygon chain
-def push_transaction():
-
-    data = {
-        "transaction_id": str(uuid.uuid4()),
-        "maker_id": str(uuid.uuid4()),
-        "taker_id": str(uuid.uuid4()),
-        "state": "MATCHED",
-    }
-    logger.bind(
-        transaction_id=data["transaction_id"],
-        maker_id=data["maker_id"],
-        taker_id=data["taker_id"],
-        state=data["state"],
-    )
-
-    logger.info("current_queue_len", queue_len=REDIS_CONN.llen(QUEUE_NAME))
-    QUEUE_DEPTH.set(REDIS_CONN.llen(QUEUE_NAME))  # type:ignore
-    # Push something half the time
-    if random.random() < 0.5:
-        # print("Pushing transaction to queue")
-        logger.debug("transaction_push")
-        REDIS_CONN.rpush(QUEUE_NAME, json.dumps(data))
-
-
-def main():
-    start_http_server(PROMETHEUS_PORT)
+async def push_transaction():
     while True:
-        push_transaction()
-        time.sleep(RUN_TIME)
-    return
+        data = {
+            "transaction_id": str(uuid.uuid4()),
+            "maker_id": str(uuid.uuid4()),
+            "taker_id": str(uuid.uuid4()),
+            "state": "MATCHED",
+        }
+        logger.bind(
+            transaction_id=data["transaction_id"],
+            maker_id=data["maker_id"],
+            taker_id=data["taker_id"],
+            state=data["state"],
+        )
+
+        logger.info("current_queue_len", queue_len=REDIS_CONN.llen(QUEUE_NAME))
+        QUEUE_DEPTH.set(REDIS_CONN.llen(QUEUE_NAME))  # type:ignore
+        # Push something half the time
+        if random.random() < 0.5:
+            # print("Pushing transaction to queue")
+            logger.debug("transaction_push")
+            REDIS_CONN.rpush(QUEUE_NAME, json.dumps(data))
+
+        await asyncio.sleep(RUN_TIME)
+
+
+async def main():
+    start_http_server(PROMETHEUS_PORT)
+    await asyncio.gather(push_transaction())
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
