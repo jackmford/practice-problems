bcede61284831376a7c6d82eb4d55a555bef3858
jackmford <jackmitchellfordyce@gmail.com>
2026-03-18 18:25:12 -0400

    kth largets

---
+++ 2026/algos/kth_largest_integer_in_a_stream.py
@@ -0,0 +1,15 @@
+class KthLargest:
+    # only ever need k integers, get rid of smallest outside of them
+    # root is always the kth largest (smallest of max size)
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        self.k = k
+        heapq.heapify(self.heap)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
