efdc08a725ee95b232e492b0bd32a0afc811cd83
jackmford <jackmitchellfordyce@gmail.com>
2026-03-23 18:00:48 -0400

    binary search

---
+++ 2026/algos/binary_search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            middle = start + ((end - start) // 2)
+            if nums[middle] == target:
+                return middle
+            if nums[middle] > target:
+                end = middle - 1
+            elif nums[middle] < target:
+                start = middle + 1
+
+        return -1
