d911944eb24f0209f3d6b4523bb8e3a5fc26461d
jackmford <jackmitchellfordyce@gmail.com>
2026-04-06 20:59:38 -0400

    daily temp stack

--- 2026/algos/daily_temperatures.py
+++ 2026/algos/daily_temperatures.py
@@ -16,3 +16,17 @@ class Solution:
                 j += 1
 
         return result
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0]*len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > stack[-1][0]
+                temp, index = stack.pop()
+                res[index] = i - index
+            stack.append((temperatures[i], i))
+            
+        return stack
+            
