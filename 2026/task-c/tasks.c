91bd991acfede25f8b03a4cf887241dcb8fbff52
jackmford <jackmitchellfordyce@gmail.com>
2026-05-08 18:32:23 -0400

    c

--- 2026/task-c/tasks.c
+++ 2026/task-c/tasks.c
@@ -17,28 +17,66 @@ struct Task {
 	char taskDesc[75];
 };
 
-int main(int argc, char *argv[]) {
+int writeToFile(char task[75]) {
+	FILE *fptr;
+	fptr = fopen("tasks.txt", "a");
 
+	if (fptr != NULL) {
+		fputs(task, fptr);
+		fputc('\n', fptr);
+        	fclose(fptr);
+	}
+
+	return 0;
+}
+
+int readFile() {
+	FILE *fptr;
+	fptr = fopen("tasks.txt", "r");
+
+	struct Task t = {0, 0};
+
+	printf("Reading file...\n");
+
+	if (fptr != NULL) {
+		while (fgets(t.taskDesc, 75, fptr)) {
+			t.taskDesc[74] = '\0';
+			printf("%s", t.taskDesc);
+		}
+        	fclose(fptr);
+	} else {
+		printf("Failed to open tasks file");
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
 	// arr is a ptr to a Task
 	// im making space for 5 Tasks with malloc
-	struct Task* arr = malloc(5 * sizeof(struct Task));
+	struct Task* arr = malloc(1 * sizeof(struct Task));
 	int pos = 0;
 
 	if (argc > 1) {
-		if (strcmp(argv[1], "add") == 0 && argc == 5) {
-			struct Task t = {atoi(argv[2]), atoi(argv[3])};
-			strncpy(t.taskDesc, argv[4], 74);
+		if (strcmp(argv[1], "add") == 0 && argc == 3) {
+			// TODO: need to get the last written task num in list to auto add new one
+			// idk if we even need this we can just display them in order
+			// status should always be 0
+			struct Task t = {0, 0};
+			strncpy(t.taskDesc, argv[2], 73);
 			t.taskDesc[74] = '\0';
+
+			int returnCode = writeToFile(t.taskDesc);
 
 			printf("Task num %d %d %s\n", t.taskNum, t.status, t.taskDesc);
 			arr[pos] = t;
 			pos = pos + 1;
 		}
-		else {
-			printf("Incorrect argument number\n");
+		else if (strcmp(argv[1], "list") == 0) {
+			readFile();
 		}
 	}
 
-	printf("%s\n", arr->taskDesc);
 	free(arr);
 }
