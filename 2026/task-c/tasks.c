4c3ceed4b85ab0620adc079c7e616188bd093cbe
jackmford <jackmitchellfordyce@gmail.com>
2026-05-11 20:06:48 -0400

    c

--- 2026/task-c/tasks.c
+++ 2026/task-c/tasks.c
@@ -30,33 +30,40 @@ int writeToFile(struct Task* t) {
 	return 0;
 }
 
-int readFile() {
+int readFile(struct Task* arr) {
 	FILE *fptr;
 	fptr = fopen("tasks.txt", "r");
+	int pos = 0;
 
 	struct Task t = {0, 0};
-
 	printf("Reading file...\n");
-
 	if (fptr != NULL) {
 		while (fgets(t.taskDesc, 75, fptr)) {
 			//t.taskDesc[74] = '\0';
-			printf("%s", t.taskDesc);
+			arr[pos] = t;
+			//printf("%s", t.taskDesc);
+
+			pos = pos+1;
+			struct Task t = {0, 0};
 		}
         	fclose(fptr);
+		return pos;
 	} else {
 		printf("Failed to open tasks file");
-		return 1;
+		return 0;
 	}
+}
 
+int deleteTask () {
 	return 0;
 }
 
 int main(int argc, char *argv[]) {
 	// arr is a ptr to a Task
 	// im making space for 5 Tasks with malloc
-	struct Task* arr = malloc(1 * sizeof(struct Task));
+	struct Task* arr = malloc(10 * sizeof(struct Task));
 	int pos = 0;
+	int taskCount = readFile(arr);
 
 	if (argc > 1) {
 		if (strcmp(argv[1], "add") == 0 && argc == 3) {
@@ -67,14 +74,17 @@ int main(int argc, char *argv[]) {
 			strncpy(t.taskDesc, argv[2], sizeof(t.taskDesc)-1);
 			t.taskDesc[sizeof(t.taskDesc) - 1] = '\0';
 
-			int returnCode = writeToFile(&t);
-
-			printf("Task num %d %d %s\n", t.taskNum, t.status, t.taskDesc);
-			arr[pos] = t;
-			pos = pos + 1;
+			writeToFile(&t);
+			printf("Task num %d %d %s written to file\n", t.taskNum, t.status, t.taskDesc);
 		}
 		else if (strcmp(argv[1], "list") == 0) {
-			readFile();
+			//int len = sizeof(arr) / sizeof(arr[0]);
+			for (int i=0; i<taskCount; i++) {
+				printf("Task num %d %d %s", arr[i].taskNum, arr[i].status, arr[i].taskDesc);
+			}
+		}
+		else if (strcmp(argv[1], "delete") == 0) {
+			deleteTask();
 		}
 	}
 
