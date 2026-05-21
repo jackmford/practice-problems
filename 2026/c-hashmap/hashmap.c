2375e02abe4b8075cfff9f77d5b4cb07a701a3ea
jackmford <jackmitchellfordyce@gmail.com>
2026-05-21 17:25:00 -0400

    hashmap

--- 2026/c-hashmap/hashmap.c
+++ 2026/c-hashmap/hashmap.c
@@ -9,6 +9,7 @@ struct Entry {
 };
 
 struct HashMap {
+	// a pointer to an array of pointers going to entries
 	struct Entry** buckets;
 	size_t capacity;
 	size_t size;
@@ -47,7 +48,41 @@ struct HashMap* hashmap_create(size_t capacity) {
 	return map;
 }
 
+void hashmap_resize(struct HashMap* map) {
+	// saving old entries
+	size_t oldCapacity = map->capacity;
+	struct Entry** oldBuckets = map->buckets;
+
+	// allocating new memory for new entries
+	// and assigning original buckets to new memory
+	map->capacity = map->capacity*2;
+	struct Entry** newEntries = calloc(map->capacity, sizeof(struct Entry *));
+	map->buckets = newEntries;
+
+	// copy the old buckets over
+	// walk linked lists
+	for (int i = 0; i < oldCapacity; i++) {
+		struct Entry* entry = oldBuckets[i];
+		while (entry != NULL) {
+			struct Entry* next = entry->next;
+			size_t new_hash = hash(entry->key, map->capacity);
+
+			// you have to set this in case there was a collision resizing, it gets saved here not overwritten
+			// if theres nothing there then next just becomes null because of calloc
+			entry->next = map->buckets[new_hash];
+			map->buckets[new_hash] = entry;
+			entry = next;
+		}
+	}
+
+	free(oldBuckets);
+	printf("New map capacity is %zu\n", map->capacity);
+}
+
 void hashmap_set(struct HashMap* map, const char* key, void* value) {
+	if ((float)map->size / map->capacity > map->load_factor) {
+		hashmap_resize(map);
+	}
 	// Put the entry on the heap
 	struct Entry* entry = malloc(sizeof(struct Entry));
 	entry->key = key;
@@ -66,21 +101,28 @@ void hashmap_set(struct HashMap* map, const char* key, void* value) {
 		map->size++;
 		return;
     	}
+		
+	while (bucket->next != NULL) {
+		printf("Collision walking on key... %s\n", key);
+		if (strcmp(bucket->key, key) == 0) {
+			printf("Updating %s at %zu\n", key, hash_key);
+			bucket->value = value;
+			free(entry);
+			return;
+		}
+		bucket = bucket->next;
+	}
 
+	// final element in bucket check
 	if (strcmp(bucket->key, key) == 0) {
 		printf("Updating %s at %zu\n", key, hash_key);
 		bucket->value = value;
 		free(entry);
 		return;
 	}
-		
-	while (bucket->next != NULL) {
-		printf("Collision walking on key... %s\n", key);
-		bucket = bucket->next;
-	}
 
-	map->size++;
 	bucket->next = entry;
+	map->size++;
 }
 
 void* hashmap_get(struct HashMap* map, const char* key) {
@@ -120,25 +162,18 @@ void hashmap_delete(struct HashMap* map, const char* key) {
 		prev = entry;
 		entry = entry->next;
 	}
-
 	printf("Key not found\n");
 }
 
 
 int main() {
-	struct HashMap* map = hashmap_create(16);
+	struct HashMap* map = hashmap_create(1);
 	if (map == NULL) {
 		printf("Allocation failed\n");
 		return 1;
 	}
 	printf("Created map with capacity %zu\n", map->capacity);
 
-	printf("%zu\n", hash("alice", 16));
-	printf("%zu\n", hash("alice", 16));
-	printf("%zu\n", hash("carol", 16));
-	printf("%zu\n", hash("jack", 16));
-	printf("%zu\n", hash("jacknum", 16));
-	
 	char str[] = "Hi\0";
 	hashmap_set(map, "jack", str);
 
@@ -165,11 +200,13 @@ int main() {
 	hashmap_set(map, "jacknum3", &num);
 	hashmap_set(map, "jacknum4", &num);
 
+	printf("Map items: %zu\n", map->size);
 	hashmap_delete(map, "jacknum4");
-	printf("Map size: %zu\n", map->size);
+	printf("Map items: %zu\n", map->size);
 
 	num = 11;
 	hashmap_set(map, "jacknum4", &num);
+	printf("Map items: %zu\n", map->size);
 
 	intpt = hashmap_get(map, "jacknum4");
 	if (intpt != NULL){
@@ -177,10 +214,7 @@ int main() {
 	}
 
 	hashmap_delete(map, "jacknum4");
-	printf("Map size: %zu\n", map->size);
-
-	//TODO implement delete
-	//TODO implement automatic resizing
+	printf("Map items: %zu\n", map->size);
 
 	free(map);
 }
